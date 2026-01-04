7bc3d835562450ae1044d5de49a8abe5538739e4
utopiaeh <utopiaeh01@gmail.com>
2026-01-04 21:32:26 +0200

    Upd handlers

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,5 +1,3 @@
-use std::fs;
-
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -29,4 +27,31 @@ pub struct Config {
     pub sync_every: u8,
     pub protected: BucketCfg,
     pub public: BucketCfg,
+}
+
+pub mod handler {
+    use axum::{Json, http::StatusCode};
+    use tokio::fs;
+
+    use crate::cfg::Config;
+
+    pub async fn root() -> &'static str {
+        "Hello World"
+    }
+
+    pub async fn get() -> Result<Json<Config>, (StatusCode, String)> {
+        match fs::read_to_string("static/config.json").await {
+            Ok(contents) => match serde_json::from_str::<Config>(&contents) {
+                Ok(c) => Ok(Json(c)),
+                Err(e) => Err((
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Invalid config format: {}", e),
+                )),
+            },
+            Err(e) => Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Could not read config: {}", e),
+            )),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 use axum::{Router, routing::get};
 
 mod cfg;
-mod handlers;
-
-use handlers::{config_route, root};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(root))
-        .route("/config", get(config_route));
+        .route("/", get(cfg::handler::root))
+        .route("/config", get(cfg::handler::get));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
