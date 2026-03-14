da75c269a372d020a3c1348a0d801f1ca3cad631
Dmitrii Cadea <dmitrii.cadea@gmail.com>
2026-03-14 22:22:05 +0200

    Fix envoy error responses to follow spec (#36)

--- crates/ratelimit-headers/src/headers.rs
+++
@@ -1,6 +0,0 @@
-// TODO: see https://datatracker.ietf.org/doc/draft-ietf-httpapi-ratelimit-headers/
-pub const RATE_LIMIT: &str = "ratelimit";
-
-pub fn rate_limit(tokens_left: u64) -> String {
-    format!("r={tokens_left}")
-}
